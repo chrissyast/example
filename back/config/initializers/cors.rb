090667e4d00473bb3b3e2884473c6dbf8a4a8839
chrissyast <chris.astles@indiecampers.com>
2020-02-25 16:22:04 +0000

    procfile for backend, add static.json, add heroku app to origins

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080', 'http://localhost:5101', 'http://localhost:5106', 'http://192.168.1.207:8080'
+    origins 'http://localhost:8080', 'http://localhost:5101', 'http://localhost:5106', 'http://192.168.1.207:8080', 'https://santadraw.herokuapp.com'
     resource '*',
       headers: :any,
       credentials: true,
