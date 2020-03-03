7fd1e5166eeac4cfdb5ea28ce241411765492d96
chrissyast <chris.astles@indiecampers.com>
2020-03-03 22:55:15 +0000

    ui changes

--- front/src/components/InputSection.vue
+++ front/src/components/InputSection.vue
@@ -1,18 +1,31 @@
 <template>
     <v-app>
         <div class="hello">
-            <form @submit.prevent="addPerson()" style="text-align: center">
-                <div class="d-flex pa-2">
-                    <v-text-field outlined true width full-width false placeholder="Enter a name here" v-model="person" hide-details true style="width: 50%; display: inline-block"/>
-                    <v-btn color="purple" v-on:click="calculate" :disabled="this.people.length < 3" style="display: inline-block; height:inherit">Let's buy some gifts!</v-btn>
+            <form @submit.prevent="addPerson()">
+                <div class="d-inline-flex input-section" style="min-width: 20px">
+                    <v-text-field class="input" outlined placeholder="Enter a name here" v-model="person" style="min-width: 20vw; min-height:50px; height:inherit" hide-details/>
+                    <v-btn class="submit-button" v-bind="props()" v-on:click="calculate" :disabled="this.people.length < 3" style=" height:inherit">Let's buy some gifts!</v-btn>
                 </div>
             </form>
             <p v-if="showValidation" class="warningMessage">
                 That name already exists
             </p>
-            <span v-for="(person, index) in people" :key="index" class="addedNames">
-              {{person}} <img src="../assets/images/delete.png" height="10" width="10" v-on:click="removePerson(index)"/><br>
-            </span>
+    <!--        TODO this should probably be its own component-->
+            <div class="d-flex justify-center flex-wrap ma-2 pa-2">
+                <v-card v-for="(person, index) in people"
+                        :key="index"
+                        class="addedNames ma-2"
+                        shaped
+                        elevation="8"
+                >
+                <v-system-bar v-bind:color="colour(index) + ' lighten-' + (index % 6)">
+                    <img src="../assets/images/delete.png" style="background-color: transparent;  filter: invert(100%);
+      -webkit-filter: invert(100%);" height="10" width="10" v-on:click="removePerson(index)" align="right"/>
+                </v-system-bar>
+                    <h1 style="padding: 0px 8px">{{person}}</h1>
+                    <br>
+                </v-card>
+            </div>
             <br>
                 <span v-if="showResults" v-for="(buyer, index) in result" :key="index">
                     <b>{{buyer}}</b> will buy for <b>{{result[buyer]}}</b><br>
@@ -35,7 +48,38 @@ import API from '../api'
               showResults: false
             }
         },
+        computed: {
+
+
+        },
         methods: {
+            props() {
+               let props = {}
+                switch(this.$vuetify.breakpoint.name) {
+                    case 'xs':
+                        props['x-small'] = true
+                        break
+                    case 'sm':
+                        props['small'] = true
+                        break
+                    case 'lg':
+                        props['large'] = true
+                        break
+                    case 'xl':
+                        props['x-large'] = true
+                        break
+                  }
+                props['color'] = this.colour()
+                return props
+            },
+            colour(index = 0) {
+
+                // TODO implement row count from https://stackoverflow.com/a/49090306/8940624 so that you always get a new colour on wrap.
+                // TODO reverse fading direct for alternate colour (i.e. go from lighter to normal)
+               if (Math.floor(index / 6) % 2)
+                return getComputedStyle(document.documentElement).getPropertyValue('--main-colour')
+                else return getComputedStyle(document.documentElement).getPropertyValue('--secondary-colour')
+            },
             addPerson() {
                 this.showValidation = false;
                 if (this.person !== '' && !this.people.includes(this.person.toString())){
@@ -68,6 +112,41 @@ import API from '../api'
 
 </script>
 
-<style scoped>
+<style>
 
+  :root {
+    --main-colour: blue;
+    --secondary-colour: orange
+  }
+
+ .addedNames {
+     height: 200px;
+     width: 200px;
+ }
+ .input-section {
+     width: 66%;
+     margin: auto
+ }
+
+ @media only screen and (max-width: 800px) {
+  /*   TODO implement sass
+       TODO try to understand https://stackoverflow.com/questions/1495407/maintain-the-aspect-ratio-of-a-div-with-css to be able to implement a max height of [80% of (vh - 150px)] but still keep width relative
+     */
+  .addedNames {
+      height: 25vw;
+      width: 25vw;
+  }
+ .input-section {
+     width: auto;
+     padding: 5px;
+ }
+ .input {
+
+ }
+
+ h1 {
+     font-size: 5vw;
+ }
+
+}
 </style>
